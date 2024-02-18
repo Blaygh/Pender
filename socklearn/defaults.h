3f0b1aede8822081bd2672590bc41d926b7fe3a4
Blaygh <blaike145@gmail.com>
2024-02-18 12:02:27 -0500

    added multithreading to for sock handling, in a loop

---
+++ socklearn/defaults.h
@@ -0,0 +1,18 @@
+#ifndef DEFAULTS_H
+#define DEFAULTS_H
+
+
+#define DEFAULT_PORT "27015"// Default Port of server
+#define BUFF_LEN 512
+
+
+#define MSG_SENT 500 //Message sent status code 
+#define MSG_DELVD 1000//Message delivered status code
+
+//@brief representing phone number of client in buff 
+#define SOCK_ID_LEN 10
+
+#define CLIENT_HANDLE_FAIL  302
+
+
+#endif
